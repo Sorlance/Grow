ba77231878ff8fddc228aba9a1562eb62a55e857
Sorlance <illilaive@gmail.com>
2023-02-24 11:13:55 +0800

    init done

---
+++ GrowReact/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <Link href="/register">
+      <a className="nav-link">注册</a>
+    </Link>
+  );
+};
+
+export default IndexPage;
